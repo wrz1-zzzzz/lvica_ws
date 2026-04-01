1406294cdf1481dba9aba8d370d1102f33d578cc
wrz1-zzzzz <914921336@qq.com>
2026-04-01 21:07:57 +0800

    初始化雷达相机标定程序

---
+++ lidar2camera/manual_calib/build/CMakeFiles/run_lidar2camera.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/run_lidar2camera"
+  "../bin/run_lidar2camera.pdb"
+  "CMakeFiles/run_lidar2camera.dir/src/run_lidar2camera.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/run_lidar2camera.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
